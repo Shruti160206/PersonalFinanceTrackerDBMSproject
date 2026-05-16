a2e5c48568366a8b349ad85c87950a73babceb7a
Latha Krishna <lathakrishna100@gmail.com>
2026-05-16 18:11:13 +0400

    add html routes and fix config

--- config.py
+++ config.py
@@ -4,21 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
 
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST", "localhost")
-    port = os.getenv("DB_PORT", "3306")
-    database_name = os.getenv("DB_NAME")
-    SECRET_KEY = 'your_secret_key_here'
-
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/userfinancetracker"
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/userfinancetracker"
     user = "root"
-    password = "password"
+    password = "latha200607"
     host = "localhost"
-    port = "3306"
+    port = 3306
     database_name = "userfinancetracker"
-    SQLALCHEMY_DATABASE_URI = f"mysql://{user}:{password}@{host}:{port}/{database_name}"
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database_name}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
