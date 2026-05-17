02b08d286951f8837ba88a56981c0816e888974b
Shruti Prakash <shruti.prakash162@gmail.com>
2026-05-16 21:28:35 -0700

    updated register for and formated html files

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
-
     user = "root"
     password = os.getenv("DB_PASSWORD", "password")
     host = "localhost"
